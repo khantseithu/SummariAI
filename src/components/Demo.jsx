5f48344bfaf7b63424265d62ebcf2506214d8248
Khant Sithu <khantseithuu@gmail.com>
2023-05-02 03:55:14 +0000

    initial demo

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,4 +1,49 @@
+import { useState } from "react";
+import { linkIcon } from "../assets";
 const Demo = () => {
-  return <div>Demo</div>;
+  const [article, setArticle] = useState({
+    url: "",
+    summary: "",
+  });
+
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      {/* Search */}
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={() => {
+            alert("submitted");
+          }}
+        >
+          <img
+            src={linkIcon}
+            alt="link-icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input
+            type="url"
+            placeholder="Paste the article link"
+            value={article.url}
+            onChange={(e) => {
+              setArticle({ ...article, url: e.target.value });
+            }}
+            required
+            className="url_input peer"
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
+          >
+            <p>↵</p>
+          </button>
+        </form>
+
+        {/* Browser History */}
+
+        {/* Summary */}
+      </div>
+    </section>
+  );
 };
 export default Demo;
